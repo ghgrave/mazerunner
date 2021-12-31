5db230d166a80a9761680eef711fd17489060710
Jon-Mikel <garrisonhgrave@yahoo.com>
2021-12-31 14:58:02 -0600

    built framework for maze.

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-// // const MAZE_WALLS = Array.from(Array(MAZE_SIZE), () => Array(MAZE_SIZE).fill(1));
-// // console.log(MAZE_WALLS)
-
-// const BUILD_MAZE_CONTAINER = (num) => {
-//   return (maze = Array.from(Array(num), () => Array(num).fill(1)));
-// };
-
-// BUILD_MAZE_CONTAINER(MAZE_SIZE);
-// BUILD_MAZE(1, MAZE_SIZE - 2);
-
-// // $("#root").html(mazeContainer(0, MAZE_SIZE));
-
-const MAZE_SIZE = 7;
+const MAZE_SIZE = 9;
 
 const MAZE_WALLS = Array.from(Array(MAZE_SIZE), () => Array(MAZE_SIZE).fill(1));
 
@@ -23,29 +11,51 @@ function Block(x, y, w) {
   this.getContent = () => this.c;
 }
 
+function block(x, y, w) {
+  return {
+    x,
+    y,
+    w,
+    c: 0,
+    setContent: (item) => (this.c = item),
+    getContent: () => this.c,
+  };
+}
+
 let maze = [...MAZE_WALLS];
 const BUILD_MAZE = (count, num) => {
   for (let i = count; i < num; i++) {
     for (let j = 0; j < num; j++) {
-      let wall = 
-        i === 0 || 
-        i === MAZE_SIZE - 1 || 
-        j === 0 || 
-        j === MAZE_SIZE - 1 ||
-        (i%2 === 0 && j%2 === 0)
-          ? 1 : 0;
-      // wall = j === 0 || j === MAZE_SIZE - 1 ? 1 : 0;
+      let wall =
+        i === 0 || // first row all walls
+        i === MAZE_SIZE - 1 || // last row all walls
+        j === 0 || // first column all walls
+        j === MAZE_SIZE - 1 || // last columns all walls
+        (i % 2 === 0 && j % 2 === 0) // default, every other odd row/col is a wall
+          ? 1 // is a wall
+          : 0; // is not a wall
 
-      maze[i][j] = new Block(i, j, wall);
-      // MAZE[i][j] = "dog";
-      // console.log(maze[i][j]);
+      // maze[i][j] = new Block(i, j, wall);
+      maze[i][j] = block(i, j, wall);
     }
   }
-
   return num !== 1 ? BUILD_MAZE((count += 1), num - 2) : maze;
 };
 
-const block1 = new Block(1, 1, 0);
-block1.setContent(10);
+const DISPLAY_WALLS = (mazeArr) => {
+  let walls = "";
+  for (let i = 0; i < mazeArr.length; i++) {
+    for (let j = 0; j < mazeArr.length; j++) {
+      walls += `<div class="walls ${
+        mazeArr[i][j].w === 1 ? "fill" : ""
+      }"></div>`;
+    }
+    walls += "<br />";
+  }
+  return walls;
+};
+
 console.log(maze);
-$("#root").html(BUILD_MAZE(0, MAZE_SIZE));
+BUILD_MAZE(0, MAZE_SIZE);
+DISPLAY_WALLS(maze);
+$("#root").html(DISPLAY_WALLS(maze));
